5310b6161994c050ea53bf64bac61b30a604f8b8
Alfonso Gonzalez <alnimag3@icloud.com>
2020-05-11 10:11:05 -0700

    remove Arrays

--- Arrays/arrays_exercise1.rb
+++
@@ -1,6 +0,0 @@
-arr = [1, 3, 5, 7, 9, 11]
-number = 3
- 
-	if arr.include?(3)
-		puts "This array includes the number #{number}"
-	end
--- Arrays/arrays_exercise7.rb
+++
@@ -1,2 +0,0 @@
-names = ["Alf", "Malcolm", "Nina", "Alexandra"]
-names.each_with_index { |val, idx| puts "#{idx+1}. #{val}"}
